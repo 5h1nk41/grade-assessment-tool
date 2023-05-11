d6b8da08eb8d1cee99f75dc996ec5ebc5043a704
Kenta Shinkai <28914836+5h1nk41@users.noreply.github.com>
2023-05-11 11:44:38 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,21 +1,45 @@
 import os
 import toml
+from openai import OpenAI
+import openai
 import streamlit as st
-from utils import generate_self_evaluation, check_requirements
 
-# 環境変数から等級要件を読み込む
-grade_requirements_str = os.environ.get("GRADE_REQUIREMENTS")
-grade_requirements = toml.loads(str(grade_requirements_str))
+# TOMLファイルから環境変数を読み込む
+config = toml.load("config.toml")
+grade_requirements = config["grade_requirements"]
+openai_api_key = config["openai_api_key"]
 
+# OpenAI APIの設定
+openai.api_key = openai_api_key
+
+# Streamlitアプリのレイアウト設定
 st.title("自己評価ツール")
+user_input = st.text_area("実績の要点を入力してください:")
 
-achievement_points = st.text_input("実績の要点を入力してください:")
-grade = st.selectbox("目指す等級を選択してください:", [2, 3, 4])
+if st.button("自己評価文章を生成"):
+    # OpenAI APIを使って自己評価文章を生成
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"以下の実績を元に、自己評価を証明する文章を生成してください。\n\n実績:\n{user_input}\n\n自己評価文章:",
+        temperature=0.5,
+        max_tokens=1000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
 
-if st.button("自己評価生成"):
-    if check_requirements(achievement_points, grade):
-        self_evaluation = generate_self_evaluation(achievement_points)
-        st.success("等級要件を満たしています。")
-        st.write(f"自己評価: {self_evaluation}")
+    generated_text = response.choices[0].text.strip()
+    st.write(generated_text)
+
+    # 生成された自己評価文章が等級要件を満たすか判断
+    grade_met = None
+    for grade, requirements in grade_requirements.items():
+        met_requirements = all(req in generated_text for req in requirements.values())
+        if met_requirements:
+            grade_met = grade
+            break
+
+    if grade_met:
+        st.success(f"生成された自己評価文章は{grade_met}の要件を満たしています。")
     else:
-        st.error("等級要件を満たしていません。要件を確認し、実績の要点を修正してください。")
+        st.warning("生成された自己評価文章は、対象となる等級の要件を満たしていません。")
