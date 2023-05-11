67269eb23017c5b2dcc326aba594a95f7e5781a0
Kenta Shinkai <28914836+5h1nk41@users.noreply.github.com>
2023-05-11 17:58:05 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,7 +12,12 @@ except KeyError:
 
 # grade_requirementsをStreamlit secretsから読み込む
 try:
-    grade_requirements = toml.loads(st.secrets["grade_requirements"])
+    grade_requirements_raw = st.secrets["grade_requirements"]
+    grade_requirements = {}
+    for grade, requirements in grade_requirements_raw.items():
+        grade_requirements[grade] = {
+            f'requirement{i}': req for i, req in enumerate(requirements.values(), 1)
+        }
 except KeyError:
     st.error("grade_requirementsが設定されていません。")
     st.stop()
