cbedade327732fe93c5dd5a9d0fcf5f6bf1ed50a
Kenta Shinkai <28914836+5h1nk41@users.noreply.github.com>
2023-05-11 18:34:06 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -26,44 +26,69 @@ except KeyError:
 st.set_page_config(page_title="自己評価作成アシスタントツール")
 st.title("自己評価作成アシスタントツール")
 
-# 目標等級を選択する
-selected_grade = st.selectbox("目指したい等級を選択してください", list(grade_requirements.keys()))
+# ドロップダウンメニューで評価対象の等級を選択
+selected_grade = st.selectbox("評価対象の等級を選択してください", list(grade_requirements.keys()))
 
-# 選択された等級の要件を表示する
-requirements = grade_requirements.get(selected_grade)
-if not requirements:
-    st.error(f"{selected_grade}に対する要件が見つかりませんでした。")
-    st.stop()
+# 選択された等級の要件を表示
+st.header(f"{selected_grade}の要件")
+for requirement, content in grade_requirements[selected_grade].items():
+    st.subheader(requirement)
+    st.write(content)
 
-st.subheader(f"{selected_grade} の等級要件")
-for key, value in requirements.items():
-    st.markdown(f'<div class="Requirement-block"><h5>{key}</h5><p class="Requirement-text">{value}</p></div>', unsafe_allow_html=True)
+# 実績入力
+st.header("実績入力")
+performance = {}
+for requirement in grade_requirements[selected_grade]:
+    performance[requirement] = st.text_area(f"{requirement} の実績を入力してください。", height=100)
 
-# 各要件に対する実績を入力する
-st.subheader(f"{selected_grade} の実績入力")
-user_achievements = {}
-for key in requirements.keys():
-    user_input = st.text_area(f"{key} の実績を入力してください", "")
-    if not user_input:
-        st.error(f"{key} の実績が入力されていません。")
-        st.stop()
-    user_achievements[key] = user_input
+# 要件判定ボタン
+assessment_results = {}
+for requirement in grade_requirements[selected_grade]:
+    if st.button(f"{requirement} の判定を実行", key=f"assessment_{requirement}"):
 
-# 各要件が実績を満たしているか判定する
-requirements_met = {}
-for key, value in requirements.items():
-    if value in user_achievements[key]:
-        requirements_met[key] = True
-    else:
-        requirements_met[key] = False
+        if not performance[requirement]:
+            st.error(f"{requirement} の実績が入力されていません。")
+        else:
+            with st.spinner("判定中..."):
+                # ここで実績と要件を比較して判定を行う
+                result = assess_performance(performance[requirement], grade_requirements[selected_grade][requirement])
 
-# 各要件の判定結果を表示する
-st.subheader(f"{selected_grade} の判定結果")
-for key, value in requirements_met.items():
-    if value:
-        st.markdown(f"{key}: **要件を満たしています**")
-    else:
-        st.markdown(f"{key}: **要件を満たしていません**")
+                if result:
+                    st.success(f"{requirement} は達成されました。")
+                else:
+                    st.error(f"{requirement} は達成されていません。")
 
+def assess_performance(performance_text, requirement_text):
+    prompt = f"以下の実績が要件を満たしているか判断してください。\n\n要件:\n{requirement_text}\n\n実績:\n{performance_text}\n\n要件を満たしている場合は「達成」、満たしていない場合は「未達成」と回答してください。回答の理由も添えてください。"
 
+                    response = openai.Completion.create(
+                    engine="text-davinci-003",
+                    prompt=prompt,
+                    max_tokens=1000,
+                    n=1,
+                    stop=None,
+                    temperature=0.5,
+                )
 
+                result_text = response.choices[0].text.strip()
+                if result_text == "達成":
+                    return True
+                else:
+                    return False
+
+# 要件判定ボタン
+assessment_results = {}
+for requirement in grade_requirements[selected_grade]:
+    if st.button(f"{requirement} の判定を実行", key=f"assessment_{requirement}"):
+
+        if not performance[requirement]:
+            st.error(f"{requirement} の実績が入力されていません。")
+        else:
+            with st.spinner("判定中..."):
+                # ここで実績と要件を比較して判定を行う
+                result = assess_performance(performance[requirement], grade_requirements[selected_grade][requirement])
+
+                if result:
+                    st.success(f"{requirement} は達成されました。")
+                else:
+                    st.error(f"{requirement} は達成されていません。")
