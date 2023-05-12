b9a8bb50cb63b39f715373da60d89b7aa53526cb
Kenta Shinkai <28914836+5h1nk41@users.noreply.github.com>
2023-05-12 10:44:30 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,18 @@ import toml
 import openai
 import streamlit as st
 
+def generate_self_evaluation(performance, grade_requirements):
+    prompt = f"実績: {performance}\n要件: {grade_requirements}\n自己評価の証明をするような納得感のある文章を生成し、等級要件を満たしているか判定してください。判定結果のロジックを説明し、満たしていない場合は、どのような実績を積み上げれば達成できるのかアドバイスを提供してください。"
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1000,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    return response.choices[0].text.strip()
+
 def assess_performance(performance, requirement):
     prompt = f"実績: {performance}\n要件: {requirement}\nこの実績は要件を満たしているか多角的な視点で厳しく判定してください。判定結果の根拠を論理的に説明してください。"
     response = openai.Completion.create(
@@ -43,9 +55,7 @@ selected_grade = st.selectbox("評価対象の等級を選択してください"
 
 # 選択された等級の要件を表示
 st.header(f"{selected_grade}の要件")
-for requirement, content in grade_requirements[selected_grade].items():
-    st.subheader(requirement)
-    st.write(content)
+selected_requirement = st.selectbox("選択したい要件を選んでください", list(grade_requirements[selected_grade].keys()))
 
 # 実績入力
 st.header("実績入力")
@@ -54,14 +64,13 @@ for requirement in grade_requirements[selected_grade]:
     performance[requirement] = st.text_area(f"{requirement} の実績を入力してください。", height=100)
 
 # 要件判定ボタン
-assessment_results = {}
-for requirement in grade_requirements[selected_grade]:
-    if st.button(f"{requirement} の判定を実行", key=f"assessment_{requirement}"):
+if st.button("自己評価を生成して判定を実行"):
 
-        if not performance[requirement]:
-            st.error(f"{requirement} の実績が入力されていません。")
-        else:
-            with st.spinner("判定中..."):
-                # ここで実績と要件を比較して判定を行う
-                result = assess_performance(performance[requirement], grade_requirements[selected_grade][requirement])
-                st.write(result)
+    missing_inputs = [req for req, value in performance.items() if not value]
+    if missing_inputs:
+        st.error(f"{', '.join(missing_inputs)} の実績が入力されていません。")
+    else:
+        with st.spinner("判定中..."):
+            # ここで実績をまとめ、自己評価の証明をするような文章を生成し、等級要件を満たしているか判定する
+            result = generate_self_evaluation(performance, grade_requirements[selected_grade])
+            st.write(result)
