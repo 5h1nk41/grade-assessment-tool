eb4dff64f0396937b42267630bbf54b1819956d3
Kenta Shinkai <28914836+5h1nk41@users.noreply.github.com>
2023-05-12 19:21:41 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -19,6 +19,22 @@ def generate_self_evaluation(performance, requirement):
     )
     return response.choices[0].text.strip()
 
+def assess_performance(performance, requirement):
+    performance_list = "\n".join([f"- {key}: {value}" for key, value in performance.items()])
+    requirement_list = "\n".join([f"- {key}: {value}" for key, value in requirement.items()])
+
+    prompt = f"以下の実績と要件を比較し、実績が要件を満たしているかどうか判断してください。\n\n実績:\n{performance_list}\n\n要件:\n{requirement_list}\n\n判断:"
+
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    return response.choices[0].text.strip()
+
 # Streamlit secretsから環境変数を読み込む
 try:
     openai.api_key = st.secrets["openai_api_key"]
